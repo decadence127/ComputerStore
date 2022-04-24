14135ec6f73642fd76421f08edb3615cf5e4284b
Kristina <isakovakristina666@gmail.com>
2022-04-24 19:23:08 +0300

    backend init commit

---
+++ ComputerStoreBackend/src/main/java/com/example/computerstorebackend/ComputerStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.computerstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerStoreBackendApplication.class, args);
+    }
+
+}
