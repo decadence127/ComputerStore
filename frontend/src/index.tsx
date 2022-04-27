1eabda137db62676c658bc17e8ea9422ee8cb7db
Evgeniy Shidlovskiy <decadence127@gmail.com>
2022-04-27 22:30:04 +0300

    fix: fix typo

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./components/App";
 import "./index.css";
 import { store } from "./redux/store";
 
