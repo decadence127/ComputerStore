2e3747522d2ed1c8c0e9e26338d6a32e87c4ef53
Evgeniy Shidlovskiy <decadence127@gmail.com>
2022-04-27 21:02:15 +0300

    feat: transfer from bootstrap to mui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,17 @@
-import React, { createContext } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
-import UserStore from "./mobx/userStore";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export const Context = createContext({
-    // Creating global context! important its out global store
-    user: null,
-});
-
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Context.Provider value={{ user: new UserStore() }}>
-                <App />
-            </Context.Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
